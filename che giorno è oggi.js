13e1fd16f0beef0e1243aa0f16ed790d018885a8
Pl4yj4ck <106487475+Pl4yj4ck@users.noreply.github.com>
2022-07-12 08:51:02 +0200

    aggiunta

---
+++ che giorno è oggi.js
@@ -0,0 +1,24 @@
+var Date = 1;
+var Giorno;
+if (Date == 1) {
+  Giorno = 'lunedi'
+}
+if (Date == 2) {
+  Giorno = 'martedì'
+}
+if (Date == 3) {
+  Giorno = 'mercoledì'
+}
+if (Date == 4) {
+  Giorno = 'giovedì'
+}
+if (Date == 5) {
+  Giorno = 'venerdì'
+}
+if (Date == 6) {
+  Giorno = 'sabato'
+}
+if (Date == 7) {
+  Giorno = 'domenica'
+}
+console.log(Giorno);
